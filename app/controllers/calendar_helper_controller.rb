774e4f9db933a68291d95d72e76191deafb22996
ycookiey <70356861+ycookiey@users.noreply.github.com>
2024-08-05 15:42:10 +0900

    update:Google Calendar APIを使用してイベントを作成する処理を追加

--- app/controllers/calendar_helper_controller.rb
+++ app/controllers/calendar_helper_controller.rb
@@ -1,13 +1,48 @@
+require 'google/apis/calendar_v3'
+
 class CalendarHelperController < ApplicationController
   def new
   end
 
   def create
-    @title = params[:title]
-    @start_time = params[:start_time]
-    @end_time = params[:end_time]
+    if current_user.nil?
+      redirect_to '/auth/google_oauth2'
+      return
+    end
+
+    service = Google::Apis::CalendarV3::CalendarService.new
+    service.authorization = client
+
+    event = Google::Apis::CalendarV3::Event.new(
+      summary: params[:title],
+      start: {
+        date_time: params[:start_time].to_datetime.rfc3339,
+        time_zone: 'Asia/Tokyo'
+      },
+      end: {
+        date_time: params[:end_time].to_datetime.rfc3339,
+        time_zone: 'Asia/Tokyo'
+      }
+    )
+
+    service.insert_event('primary', event)
 
     flash[:notice] = 'イベントがGoogleカレンダーに追加されました'
     redirect_to calendar_helper_new_path
+  rescue Google::Apis::AuthorizationError
+    redirect_to '/auth/google_oauth2'
+  end
+
+  private
+
+  def client
+    Signet::OAuth2::Client.new(
+      client_id: ENV['GOOGLE_CLIENT_ID'],
+      client_secret: ENV['GOOGLE_CLIENT_SECRET'],
+      token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+      access_token: current_user.oauth_token,
+      refresh_token: current_user.refresh_token,
+      expires_at: current_user.oauth_expires_at.to_i
+    )
   end
 end
