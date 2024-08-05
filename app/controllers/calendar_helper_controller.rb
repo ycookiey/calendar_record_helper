42fb6f0f48d8d3a8ff8e614f8d4dfce67213c099
ycookiey <70356861+ycookiey@users.noreply.github.com>
2024-08-05 14:57:40 +0900

    update:コントローラーの雛形を定義

--- app/controllers/calendar_helper_controller.rb
+++ app/controllers/calendar_helper_controller.rb
@@ -3,5 +3,11 @@ class CalendarHelperController < ApplicationController
   end
 
   def create
+    @title = params[:title]
+    @start_time = params[:start_time]
+    @end_time = params[:end_time]
+
+    flash[:notice] = 'イベントがGoogleカレンダーに追加されました'
+    redirect_to calendar_helper_new_path
   end
 end
