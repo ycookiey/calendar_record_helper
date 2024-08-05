02b32035603cc9d83a0138b36048911db2cba2d5
ycookiey <70356861+ycookiey@users.noreply.github.com>
2024-08-05 18:03:15 +0900

    update:User モデルに refresh_token カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_05_063742) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_05_090212) do
   create_table "users", force: :cascade do |t|
     t.string "provider"
     t.string "uid"
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_05_063742) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "refresh_token"
   end
 
 end
